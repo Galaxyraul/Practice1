9b0868864d1645f7fcb1340a7f8448278aed1fac
Galaxyraul <cocolete03@gmail.com>
2022-02-14 19:05:16 +0100

    most expensive

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -9,7 +9,7 @@ struct Vehicle{
     int ManufacturerYear = 0;
     float Price = 0;
 };
-void kbRead(Vehicle &v,int size){
+void kbRead(Vehicle &v,const int size){
     for(int i = 0; i < size;++i){
         do {
             cout << "Introduce the brand of the vehicle";
@@ -32,11 +32,11 @@ void kbRead(Vehicle &v,int size){
     }
 }
 
-void kbRead(Vehicle *v,int size){
+void kbRead(Vehicle *v,const int size){
     for(int i = 0; i < size; ++i){
         do {
             cout << "Introduce the brand of the vehicle";
-            cin >> v->Brand;
+            cin >> (v+i)->Brand;
         } while ((v->Brand.length() < 3) || (v->Brand.length() > 20));
         do {
             cout << "Introduce the model of the vehicle";
@@ -55,25 +55,34 @@ void kbRead(Vehicle *v,int size){
     }
 }
 
-void showOnScreen(const Vehicle &v){
+void showOnScreen(const Vehicle &v,const int size){
     cout << "Brand:" << v.Brand << endl;
     cout << "Model:" << v.Model << endl;
     cout << "Plate:" << v.Plate << endl;
     cout << "Manufacturer year:" << v.ManufacturerYear << endl;
     cout << "Price:" << v.Price << endl;
+    cout << "";
 };
 
-void showOnScreen(const Vehicle *v){
+void showOnScreen(const Vehicle *v,const int size){
     cout << "Brand:" << v->Brand << endl;
     cout << "Model:" << v->Model << endl;
     cout << "Plate:" << v->Plate << endl;
     cout << "Manufacturer year:" << v->ManufacturerYear << endl;
     cout << "Price:" << v->Price << endl;
+    cout << "";
 };
-
+int findMostExpensive(Vehicle *v,const int size){
+    int posMostExpensive = 0;
+    for (int i = 1; i < size;++i){
+        posMostExpensive = (v[posMostExpensive].Price > v[i].Price)? posMostExpensive:i;
+    }
+    return posMostExpensive;
+}
 
 int main() {
     int size = 0;
+    int posMostExpensive;
     Vehicle a = {"mercedes","hola","123456",1907,200};
     Vehicle list[MAX_VEHICLES];
     do{
@@ -81,6 +90,9 @@ int main() {
         cin >> size;
     }while(size > MAX_VEHICLES);
     kbRead(list,size);
-    showOnScreen(a);
+    posMostExpensive = findMostExpensive(list,size);
+    showOnScreen(list,size);
+    cout << "The most expensive vehicle data is:" << endl;
+    showOnScreen(list[posMostExpensive],size);
     return 0;
 }
