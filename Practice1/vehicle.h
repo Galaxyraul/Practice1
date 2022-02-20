3ee83793fd9131d74f3fce32bb4646f24ce6f713
Galaxyraul <cocolete03@gmail.com>
2022-02-20 19:28:26 +0100

    Borrador1

--- Practice1/vehicle.h
+++ Practice1/vehicle.h
@@ -5,37 +5,39 @@
 #ifndef PRACTICE1_VEHICLE_H
 #define PRACTICE1_VEHICLE_H
 #include <string>
+
 using namespace std;
+namespace v {
+    struct Vehicle {
+        string Brand ;
+        string Model ;
+        string Plate ;
+        int ManufacturerYear = 0;
+        float Price = 0;
+    };
 
-struct Vehicle{
-    string Brand ="";
-    string Model ="";
-    string Plate ="" ;
-    int ManufacturerYear = 0;
-    float Price = 0;
-};
-void kbRead(Vehicle &v,int size);
-void kbRead(Vehicle *v,int size);
-void showOnScreen(const Vehicle &v,int size);
-void showOnScreen(const Vehicle *v,int size);
-int findMostExpensive(const Vehicle *v,int size);
-void platesSECharacter (const Vehicle *v,int size);
-void platesPalyndrome (const Vehicle *v,int size);
+    void kbRead(Vehicle &v);
 
-class vehicle {
-private:
-    string _brand ="";
-    string _model ="";
-    string _plate="";
-    int _manufacturerYear = 0;
-    float _price = 0;
-public:
-    void setBrand ();
-    void setModel ();
-    void setPlate ();
-    void setManufacturerYear ();
-    void setPrice();
-};
+    void kbRead(Vehicle *v, int size);
+
+    void showOnScreen(const Vehicle &v);
+
+    void showOnScreen(const Vehicle *v, int size);
+
+    int findMostExpensive(const Vehicle *v, int size);
+
+    void platesSECharacter(const Vehicle *v, int size);
+
+    void platesPalyndrome(const Vehicle *v, int size);
+
+    int fillVector (Vehicle v[], int lenV);
+
+    void showOnScreen ( Vehicle v[], int lenV );
+
+    int maxPrice (const  Vehicle v[], int lenV );
+
+    int searchByPlate ( string plateNumber, const Vehicle v[], int lenV );
+}
 
 
 #endif //PRACTICE1_VEHICLE_H
