212313ae9f851d4f4545982d8ff92a7adf5e8916
Galaxyraul <cocolete03@gmail.com>
2022-02-26 21:14:46 +0100

    djutils

--- Practice2/djutils.h
+++ Practice2/djutils.h
@@ -4,7 +4,17 @@
 
 #ifndef PRACTICE2_DJUTILS_H
 #define PRACTICE2_DJUTILS_H
+#include "Fecha.h"
+#include "Garito.h"
+#include "Temazo.h"
 
+void showTemazo(const Temazo& temazo );
+
+void showGarito(const Garito& garito );
+
+void showFecha(const Fecha& fecha );
+
+void changeGarito(Garito& garito);
 
 class djutils {
 
