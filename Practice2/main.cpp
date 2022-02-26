9d47b5924cb1cb0207b1a21db08f935ff548d37e
Galaxyraul <cocolete03@gmail.com>
2022-02-26 21:24:38 +0100

    main_a

--- Practice2/main.cpp
+++ Practice2/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "djutils.h"
+#include "Fecha.h"
+#include "Garito.h"
+#include "Temazo.h"
+#include <iostream>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Temazo t1;
+    Temazo t2("I hate everything about you","Three days grace",231);
+
+    Garito g1("Moët","Av.Andalucía nº 30");
+    Garito g2 (g1);
+
+    Fecha f1;
+    Fecha f2(13,4,2003);
 }
