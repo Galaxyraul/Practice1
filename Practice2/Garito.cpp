86f1798960ac6ac178c36d3ead74f1febe32c68c
Galaxyraul <cocolete03@gmail.com>
2022-02-26 20:53:24 +0100

    Garito

--- Practice2/Garito.cpp
+++ Practice2/Garito.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "Garito.h"
+
+Garito::Garito(const std::string &name, const std::string &address) : _name(name), _address(address) {}
+
+Garito::Garito(const Garito& orig) : _name(orig._name), _address(orig._address) {}
+
+Garito::~Garito() {
+
+}
+
+const std::string &Garito::getName() const {
+    return _name;
+}
+
+void Garito::setName(const std::string &name) {
+    if (name == ""){
+        throw std::string ("The name cannot be the empty chain");
+    }
+    this->_name = name;
+}
+
+const std::string &Garito::getAddress() const {
+    return _address;
+}
+
+void Garito::setAddress(const std::string &address) {
+    if (address == ""){
+        throw std::string ("The address cannot be the empty chain");
+    }
+    this->_address = address;
+}
+
+
--- Practice2/Garito.h
+++ Practice2/Garito.h
@@ -4,9 +4,26 @@
 
 #ifndef PRACTICE2_GARITO_H
 #define PRACTICE2_GARITO_H
-
+#include <string>
 
 class Garito {
+private:
+    std::string _name = "";
+    std::string _address = "";
+public:
+    Garito(const std::string &name, const std::string &address);
+
+    Garito(const Garito& orig);
+
+    virtual ~Garito();
+
+    const std::string &getName() const;
+
+    void setName(const std::string &name);
+
+    const std::string &getAddress() const;
+
+    void setAddress(const std::string &address);
 
 };
 
