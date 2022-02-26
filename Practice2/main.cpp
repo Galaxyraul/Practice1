e1b7f72e815b3032df609a5afa58c88a7a72698b
Galaxyraul <cocolete03@gmail.com>
2022-02-26 21:45:49 +0100

    AllDone

--- Practice2/main.cpp
+++ Practice2/main.cpp
@@ -3,10 +3,12 @@
 #include "Fecha.h"
 #include "Garito.h"
 #include "Temazo.h"
-#include <iostream>
+
 
 
 int main() {
+    bool end = false;
+    int answer;
     Temazo t1;
     Temazo t2("I hate everything about you","Three days grace",231);
 
@@ -15,17 +17,39 @@ int main() {
 
     Fecha f1;
     Fecha f2(13,4,2003);
-
-    showTemazo(t1);
-    showTemazo(t2);
-    showGarito(g1);
-    showGarito(g2);
-    showFecha(f1);
-    showFecha(f2);
-    try{
-        changeGarito(g2);
-        showGarito(g2);
-    }catch (std::string& e){
-        std::cerr << e;
+    while(!end){
+        std::cout <<"1.-Show the data of the temazos." <<std::endl
+        <<"2.-Show the data of the garitos" <<std::endl
+        <<"3.-Show the data of the fechas"<< std::endl
+        <<"4.-Modify the data of the second garito"<<std::endl
+        <<"5.-End the apliccation" <<std::endl<<
+        "What do you want to do:" ;
+        std::cin >> answer;
+       switch (answer) {
+            case 1:
+                showTemazo(t1);
+                showTemazo(t2);
+                break;
+            case 2:
+                showGarito(g1);
+                showGarito(g2);
+                break;
+            case 3:
+                showFecha(f1);
+                showFecha(f2);
+                break;
+            case 4:
+                try{
+                    changeGarito(g2);
+                    showGarito(g2);
+                }catch (std::string& e){
+                    std::cerr << e;
+                }
+                break;
+            case 5:
+                end = true;
+                break;
+        }
     }
+
 }
