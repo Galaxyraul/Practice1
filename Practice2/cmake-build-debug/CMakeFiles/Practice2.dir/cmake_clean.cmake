dee95401fdbbe8261bb9fcc440c4a2f31f00cc94
Galaxyraul <cocolete03@gmail.com>
2022-03-12 12:35:53 +0100

    Point 1 P4

---
+++ Practice2/cmake-build-debug/CMakeFiles/Practice2.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practice2.dir/Fecha.cpp.obj"
+  "CMakeFiles/Practice2.dir/Fecha.cpp.obj.d"
+  "CMakeFiles/Practice2.dir/Garito.cpp.obj"
+  "CMakeFiles/Practice2.dir/Garito.cpp.obj.d"
+  "CMakeFiles/Practice2.dir/Temazo.cpp.obj"
+  "CMakeFiles/Practice2.dir/Temazo.cpp.obj.d"
+  "CMakeFiles/Practice2.dir/djutils.cpp.obj"
+  "CMakeFiles/Practice2.dir/djutils.cpp.obj.d"
+  "CMakeFiles/Practice2.dir/main.cpp.obj"
+  "CMakeFiles/Practice2.dir/main.cpp.obj.d"
+  "Practice2.exe"
+  "Practice2.exe.manifest"
+  "Practice2.pdb"
+  "libPractice2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practice2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
