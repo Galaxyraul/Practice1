ef826c0750ac276841d4fd33d9404b809249e17c
GalaxyLaptop <cocolete03@gmail.com>
2022-03-21 11:33:59 +0100

    Points 1 & 2

--- Practice5/Piloto.h
+++ Practice5/Piloto.h
@@ -9,6 +9,8 @@
 #define PILOTO_H
 
 #include <string>
+#include "StarFighter.h"
+#include "Droide.h"
 
 /**
  * @brief
@@ -23,25 +25,56 @@ class Piloto
       int _numMisiones = 0;        ///< Número de misiones en que ha participado
       long _fechaUltimaMision = 0;        ///< Fecha estelar de su última misión
       std::string _incidenciasUltimaMision; ///< Incidencias reportadas por el piloto en su última misión.
+      StarFighter *myStarFighter = nullptr;
+      bool onService = false;
+      Droide *support[10];
+      int _numberOfDroids = 0;
 
    public:
       Piloto ( );
-      Piloto ( std::string nombre );
+
+      Piloto ( std::string nombre,int numberOfDorids);
+
       Piloto ( const Piloto& orig );
+
       virtual ~Piloto ( );
+
       void setNumMisiones ( int numMisiones );
+
       int getNumMisiones ( ) const;
+
       void setNacionalidad ( std::string nacionalidad );
+
       std::string getNacionalidad ( ) const;
+
       void setNombre ( std::string nombre );
+
       std::string getNombre ( ) const;
+
       int getIdP ( ) const;
+
       void setIncidenciasUltimaMision ( std::string incidenciasUltimaMision );
+
       std::string getIncidenciasUltimaMision ( ) const;
+
       void setFechaUltimaMision ( long fechaUltimaMision );
+
       long getFechaUltimaMision ( ) const;
+
       std::string toCSV () const;
+      
       Piloto& operator= ( const Piloto& otro );
+
+      const void adquirirStarfighter(StarFighter *nuevo);
+
+      const void despegar();
+
+      const void aterrizar();
+
+      const void addDroid();
+
+      const void droidCasualty(int pos);
+
 };
 
 #endif /* PILOTO_H */
