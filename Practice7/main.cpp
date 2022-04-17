a1d429efaf96f78b99908d30f1285ac77c7f9c81
Galaxyraul <cocolete03@gmail.com>
2022-04-17 17:53:51 +0200

    Point 7 needs documentation

--- Practice7/main.cpp
+++ Practice7/main.cpp
@@ -18,7 +18,7 @@ void visualiza( Mutante& m) {
               << m.toCSV() << std::endl;
     std::cout << "  Poderes : " << std::endl;
     int k = m.getNumOfPowers();
-    for(int i = 0; i < m.MAX_PODERES && k>0; ++i){
+    for(int i = 0; i <= m.MAX_PODERES && k>0; ++i){
         if(m.getPower(i)!= nullptr){
             cout << m.getPower(i)->toCSV() << endl;
             --k;
@@ -43,7 +43,7 @@ void visualiza( EquipoMutante& eq) {
 
 }
 
-void StoreMutants(Mutante* v[], int tamV, std::string fileName){
+void storeMutants(Mutante* v[], int tamV, std::string fileName){
     ofstream f;
     f.open(fileName.c_str());
     if(f.good()){
@@ -62,26 +62,73 @@ void StoreMutants(Mutante* v[], int tamV, std::string fileName){
 int main(int argc, char** argv) {
 
     
-   // Crea un array de 5 punteros a mutantes, asignándoles los valores a sus
-   // atributos directamente en el código fuente
+   Mutante* v[5];
+   for (int i = 0; i < 5; ++i){
+       v[i] = new Mutante ();
+   }
+   v[0]->setApodo("Larry");
+   v[0]->setFechaDeNacimiento(13042003);
+   v[0]->setNacionalidad("Spanish");
+   v[0]->setNombreReal("Pepe");
 
-    
-   // Crea dos equipos mutantes, asignando al primero los mutantes que ocupan
-   // las posiciones pares del array, y al segundo los de las posiciones impares
-    
-    
-   // Añade varios poderes a cada mutante, utilizando los métodos ya
-   // implementados: usando datos o objetos de tipo Poder
-   // Muestra por la consola la información de cada equipo mutante en formato CSV
+   v[1]->setApodo("B.I.G");
+   v[1]->setFechaDeNacimiento(01042003);
+   v[1]->setNacionalidad("Spanish");
+   v[1]->setNombreReal("Antonio");
 
-    
-   //Guardamos datos de mutantes en fichero CSV
+   v[2]->setApodo("N");
+   v[2]->setFechaDeNacimiento(07042003);
+   v[2]->setNacionalidad("French");
+   v[2]->setNombreReal("Engolo");
+
+   v[3]->setApodo("NightK");
+   v[3]->setFechaDeNacimiento(18509300);
+   v[3]->setNacionalidad("Japanese");
+   v[3]->setNombreReal("Yumiko");
+
+   v[4]->setApodo("CaptainSun");
+   v[4]->setFechaDeNacimiento(15082005);
+   v[4]->setNacionalidad("American");
+   v[4]->setNombreReal("Peggy");
+
+   try{
+       v[0]->addPower("PyroKinesis","Ability to control fire","Effective on almost every mutant but water related",100);
+       v[1]->addPower("WaterBending","Ability to control water","Effective on fire related mutants",75);
+       v[2]->addPower("Invisibility","Ability to become invisible to humans","Effective every mutant but tracking related",2);
+       v[3]->addPower("Teleportation","Ability to appear in whichever place the user wants","Effective on no mutant",0);
+       v[4]->addPower("Flight","Ability to fly ","Effective on no mutant",20);
+   }catch (std::string &e){
+       cerr << e;
+   }
+   EquipoMutante t1;
+   EquipoMutante t2;
+   try{
+       for(int i = 0; i < 5;++i){
+           if(i%2 == 0){
+               t1.addMutante(v[i]);
+           } else{
+               t2.addMutante(v[i]);
+           }
+       }
+   }catch (std::length_error &e){
+       cerr << e.what();
+   }
+try {
+    visualiza(t1);
+    visualiza(t2);
+}catch (std::invalid_argument &e){
+    cerr << e.what();
+}
+    storeMutants(v,5,"List.csv");
     
     
    // Destruye todos los objetos creados en memoria dinámica antes de la
    // finalización del programa
 
-    
+    for(int i = 0 ; i  < 5; ++i){
+        delete v[i];
+        v[i] = nullptr;
+    }
     
     return 0;
 }
